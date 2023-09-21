e70f499573477458616997845a51fbc675405c3d
abdelrahman-2513 <ab19622001@gmail.com>
2023-09-20 19:17:15 -0700

    commit

--- get_path.c
+++ get_path.c
@@ -19,26 +19,5 @@ char *get_path(char *command)
 			return (executable_command);
 		}
 	}
-	else
-	{
-
-		char *path = getenv("PATH");
-		char *token = strtok(path, ":");
-
-		while (token != NULL)
-		{
-			char *command_path = (char *)malloc(100);
-
-			str_cpy(command_path, token);
-			str_cat(command_path, "/");
-			str_cat(command_path, command);
-
-			if (access(command_path, X_OK) == 0)
-			{
-				return (command_path);
-			}
-			token = strtok(NULL, ":");
-		}
-	}
 	return (NULL);
 }
